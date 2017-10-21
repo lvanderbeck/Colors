383742d46b0197210a249e6fac499df6e3c45fa1
Logan VanDerBeck <logan.vanderbeck@gmail.com>
2017-10-20 21:30:59 -0500

    Initial commit

---
+++ ColorsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ColorsTableViewCell.swift
+//  Colors
+//
+//  Created by Logan VanDerBeck on 10/20/17.
+//  Copyright © 2017 Logan VanDerBeck. All rights reserved.
+//
+
+import UIKit
+
+class ColorsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ColorsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ColorsViewController.swift
+//  Colors
+//
+//  Created by Logan VanDerBeck on 10/20/17.
+//  Copyright © 2017 Logan VanDerBeck. All rights reserved.
+//
+
+import UIKit
+
+class ColorsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
